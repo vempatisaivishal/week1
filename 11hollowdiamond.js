3fd727c1e391de32c7e5bbbfb989e6a4e92711ad
vempatisaivishal <106369240+vempatisaivishal@users.noreply.github.com>
2023-07-06 23:34:15 +0530

    Add files via upload
    hollow diamond

---
+++ 11hollowdiamond.js
@@ -0,0 +1,31 @@
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+    for (let j = n; j > i; j--) {
+        string += " ";
+    }
+    for (let k = 0; k < i * 2 - 1; k++) {
+        if (k === 0 || k === 2 * i - 2) {
+            string += "*";
+        }
+        else {
+            string += " ";
+        }
+    }
+    string += "\n";
+}
+for (let i = 1; i <= n - 1; i++) {
+    for (let j = 0; j < i; j++) {
+        string += " ";
+    }
+    for (let k = (n - i) * 2 - 1; k >= 1; k--) {
+        if (k === 1 || k === (n - i) * 2 - 1) {
+            string += "*";
+        }
+        else {
+            string += " ";
+        }
+    }
+    string += "\n";
+}
+console.log(string);
